0b5d28a70ae0a67a6f4479992d4c617f68808d5d
Pablo Ariel De Los Santos <pdelossantos@nosis.com>
2024-09-17 15:40:00 -0300

    Operadores de comparacion

---
+++ 05-type-casting-coercion/type-casting.js
@@ -0,0 +1,14 @@
+// Lenguajes de programacion
+
+// Compilados -> C, C++, Rust, Go, Swift. Se traducen antes de la ejecución. Chequeo estático de tipos
+
+// Interpretados -> JavaScript, Python, Ruby, PHP. Se traducen durante la ejecución. Chequeo dinámico de tipos.
+// JS no sabe que tipos de datos hasta que se ejecuta el programa. Por eso se dice que es debilmente tipado.
+
+// Conversión
+
+// Implícita
+// 2 + true
+
+// Explícita
+// string(), number(), boolean()
---
+++ 06-ejecucion-condicional/comparacion.js
@@ -0,0 +1,16 @@
+// Operadores de comparación
+
+// ==
+// === -> Igual en valor y en tipo de dato
+// != -> Diferente a
+// !== -> Estrictamente diferente (en valor y en tipo de dato)
+// > mayor que -> >= mayor o igual
+// < menor que -> <= menor o igual
+
+const a = 10
+const b = 20
+const c = "10"
+
+a == b
+a === c
+a != b
