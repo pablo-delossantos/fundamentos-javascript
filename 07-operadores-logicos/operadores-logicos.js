90d86123726b1e995e472da2fa71797077667af8
Pablo de los Santos <estudiodedalo@gmail.com>
2024-09-18 08:59:55 -0300

    estructuras de control

---
+++ 07-operadores-logicos/operadores-logicos.js
@@ -0,0 +1,9 @@
+// Operadores lógicos
+
+// && -> and
+// || -> or
+// ! -> not
+
+const a = 10
+const b = 20
+const c = "10"
---
+++ 08-estructuras-de-control/if.js
@@ -0,0 +1,17 @@
+// Condicional if -> Si
+
+// if (let === algo) {
+//     código
+// } else if ( let === otracosa){
+//     algomas
+// } else {
+//     otracosa
+// }
+
+// else if -> Entonces si
+
+let nombre = "Diego"
+
+if (nombre == "Diego") {
+    console.log("Hola Diego")
+}
