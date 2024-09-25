9415a8a111d27baead7ca0cc0c1a4eede3fa0a06
Pablo de los Santos <estudiodedalo@gmail.com>
2024-09-25 19:04:28 -0300

    clase 12

--- 11-estructuras-de-control/if.js
+++ 11-estructuras-de-control/if.js
@@ -12,8 +12,10 @@
 
 let nombre = "Pablo"
 
-if (nombre == "Diego") {
-    console.log("Hola Diego")
+if (nombre === "Diego") {
+    console.log("Hola Diego");
+} else if (nombre === "Nico") {
+    console.log("Hola Nico");
 } else {
     console.log("Nombre no encontrado")
 }
